138d4215e9642c2db6f90cf78924021215630dc9
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-03-07 21:27:32 +0900

    20220307 TIL

--- NEXT.js/next-app/pages/posts/[id].jsx
+++ NEXT.js/next-app/pages/posts/[id].jsx
@@ -1,20 +1,42 @@
-import { useRouter } from "next/router";
 import Link from "next/link";
 import Head from "next/head";
 import Layout from "../../components/layout";
+import Date from "../../components/date";
 
-export default function FirstPost() {
-  const router = useRouter();
-  console.log(router);
+import { getAllPostIds, getPostData } from "../../libs/posts";
+
+import utilStyles from "../../styles/utils.module.css";
+
+export default function Post({ postData }) {
   return (
     <Layout>
       <Head>
-        <title>first-post</title>
+        <title>{postData.title}</title>
       </Head>
-      <h1>first-post</h1>
-      <Link href="/">
-        <a>back to home</a>
-      </Link>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
