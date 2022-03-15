6982a7014aa304b54d287436b294a65abd837e71
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-03-15 21:15:41 +0900

    20220315 TIL

--- NEXT.js/next-app/pages/posts/[id].jsx
+++ NEXT.js/next-app/pages/posts/[id].jsx
@@ -14,6 +14,7 @@ export default function Post({ postData }) {
         <title>{postData.title}</title>
       </Head>
       <article>
+        <h1>jimmy</h1>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
